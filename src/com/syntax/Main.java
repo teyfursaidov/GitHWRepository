03297b2bea62c9b2d4417246d1b30e122010a97f
teyfursaidov <teyfursaidov1991@gmail.com>
2022-05-15 15:21:31 +0300

    HW Commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,19 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*HW1:
+    1.create a new IJ project
+    2.add code in it that prints the numbers from 1 to 100
+    3.send this code to github
+	* */
+
+        for(int i=0; i<=100; i++){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
